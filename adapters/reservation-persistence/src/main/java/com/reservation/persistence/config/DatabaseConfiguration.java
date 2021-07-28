5c7d70c280742f50b9f451e212b27620fc680c01
Ignatovw99 <lyuboslavw@gmail.com>
2021-07-28 10:59:09 +0300

    property type constraints added

--- adapters/reservation-persistence/src/main/java/com/reservation/persistence/config/DatabaseConfiguration.java
+++ adapters/reservation-persistence/src/main/java/com/reservation/persistence/config/DatabaseConfiguration.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import javax.sql.DataSource;
 
 @Configuration
+@ComponentScan(basePackages = {"com.reservation.persistence"})
 @EnableJpaRepositories(basePackages = {"com.reservation.persistence.core.repository"})
 @EntityScan(basePackages = {"com.reservation.application.domain.entity", "com.reservation.persistence.core.entity"})
 @EnableConfigurationProperties(LiquibaseProperties.class)
