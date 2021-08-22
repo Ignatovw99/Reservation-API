547bdf9dac1959f096a16765b7f9a3cf19d63873
Ignatovw99 <lyuboslavw@gmail.com>
2021-08-22 14:16:57 +0300

    logging added in UserPersistenceAdapter class

--- adapters/reservation-persistence/src/main/java/com/reservation/persistence/adapter/UserPersistenceAdapter.java
+++ adapters/reservation-persistence/src/main/java/com/reservation/persistence/adapter/UserPersistenceAdapter.java
@@ -6,25 +6,30 @@ import com.reservation.application.port.out.PersistUserPort;
 import com.reservation.common.component.PersistenceAdapter;
 import com.reservation.persistence.core.repository.AppUserJpaRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @PersistenceAdapter
 @RequiredArgsConstructor
+@Slf4j
 class UserPersistenceAdapter implements FindUserPort, PersistUserPort {
 
     private final AppUserJpaRepository appUserRepository;
 
     @Override
     public AppUser findUserByUsername(String username) {
+        log.info("Fetching user by username: {}", username);
         return appUserRepository.findByUsername(username);
     }
 
     @Override
     public AppUser findUserByEmail(String email) {
+        log.info("Fetching user by email: {}", email);
         return appUserRepository.findByEmail(email);
     }
 
     @Override
     public AppUser saveUser(AppUser user) {
+        log.info("Persisting user: {}", user);
         return appUserRepository.saveAndFlush(user);
     }
 }
